33a392301cb97c490a764fca91ee190b3d475fdd
Jatin Vaidya <jatin.vaidya@gmail.com>
2022-02-22 13:20:02 +1100

    useRT false

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const providerConfig = {
   ...(config.audience ? { audience: config.audience } : null),
   redirectUri: window.location.origin,
   onRedirectCallback,
-  useRefreshTokens: true
+  useRefreshTokens: false,
 };
 
 ReactDOM.render(
